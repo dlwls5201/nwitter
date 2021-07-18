751045a2a3795328556ea151c5471dbce0b7faad
dlwls5201 <dlwls5201@naver.com>
2021-07-18 22:50:36 +0900

    project setup

---
+++ src/components/Router.js
@@ -0,0 +1,27 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import {HashRouter as Router, Route, Switch} from "react-router-dom";
+import Home from "../routes/Home";
+import Auth from "../routes/Auth";
+
+const AppRouter = ({isLoggedIn}) => {
+    return (
+        <Router>
+            <Switch>
+                {isLoggedIn ? (
+                    <>
+                        <Route exact path="/">
+                            <Home/>
+                        </Route>
+                    </>
+                ) : (
+                    <Route exact path="/">
+                        <Auth/>
+                    </Route>
+                )
+                }
+            </Switch>
+        </Router>
+    )
+}
+export default AppRouter;
